29881012973fb2ba6cc2d3c28c72077b2993587e
Bikal <bikaladhikari5@gmail.com>
2024-03-27 17:39:31 +1100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My favourite foods</h1>
+      <ul>
+        <li>Chicken curry</li>
+        <li>Goat Curry</li>
+        <li>Lentils</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
